607ed610426600c1628034f40ace213f39a934c9
Johan Smet <johan.smet@justcode.be>
2018-03-08 22:46:44 +0100

    UI to control the AudioPlayer

--- src/main_gui.cpp
+++ src/main_gui.cpp
@@ -1,8 +1,16 @@
 #include "main_gui.h"
 #include "imgui/imgui.h"
 
+#include "audio_player.h"
+#include <math.h>
+
 static const int NOTE_MIN = 24;
 static const int NOTE_MAX = 96;
+
+float note_to_frequency(int p_note)
+{
+	return (float)(440.0f * pow(2.0, (p_note - 69.0) / 12.0));
+}
 
 int next_window_dims(int y_pos, int height)
 {
@@ -18,6 +26,7 @@ void main_gui()
 	bool update_square = false;
 	bool update_saw = false;
 
+	// gui
 	int window_y = next_window_dims(0, 80);
 	ImGui::Begin("Sine Wave Generator");
 		
@@ -50,4 +59,22 @@ void main_gui()
 		update_saw |= ImGui::SliderFloat("Volume", &saw_volume, 0.0f, 1.0f);
 
 	ImGui::End();
+
+	// audio control
+	static AudioPlayer	player;
+
+	if (update_sine)
+	{
+		player.oscillator_change(AudioPlayer::SineWave, note_to_frequency(sine_note), sine_volume);
+	}
+
+	if (update_square)
+	{
+		player.oscillator_change(AudioPlayer::SquareWave, note_to_frequency(square_note), square_volume);
+	}
+
+	if (update_saw)
+	{
+		player.oscillator_change(AudioPlayer::SawTooth, note_to_frequency(saw_note), saw_volume);
+	}
 }
